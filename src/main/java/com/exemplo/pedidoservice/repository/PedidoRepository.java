10ab7f4a257d4875c5c33634119ae868bad3a31d
Joey alan <joeyalan50@gmail.com>
2025-06-07 23:27:48 -0300

    ###Changed

--- src/main/java/com/exemplo/pedidoservice/repository/PedidoRepository.java
+++ src/main/java/com/exemplo/pedidoservice/repository/PedidoRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface PedidoRepository extends MongoRepository<Pedido, String> {
     List<Pedido> findByClienteId(String clienteId);
+    List<Pedido> findAll();
 }
