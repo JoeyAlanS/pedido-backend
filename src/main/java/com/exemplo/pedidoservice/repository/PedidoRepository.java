2169ee1f7244fdbb27fe055939eb0acae3fdcd30
Joey alan <joeyalan50@gmail.com>
2025-06-07 23:20:19 -0300

    ###Changed

--- src/main/java/com/exemplo/pedidoservice/repository/PedidoRepository.java
+++ src/main/java/com/exemplo/pedidoservice/repository/PedidoRepository.java
@@ -2,9 +2,11 @@ package com.exemplo.pedidoservice.repository;
 
 import com.exemplo.pedidoservice.model.Pedido;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PedidoRepository extends MongoRepository<Pedido, String> {
     List<Pedido> findByClienteId(String clienteId);
 }
